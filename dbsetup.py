93ff1dc9f6fa70a06d9c1961b44430d83a8c0a5b
RJ <rjm6823@rit.edu>
2016-10-25 12:29:31 -0400

    Commented functions for dbsetup; fixed INNODB warning, replaced disable/enable keys with disabling/enabling unique and foreign key checks

--- dbsetup.py
+++ dbsetup.py
@@ -3,11 +3,13 @@ from music_logger import app
 from os.path import abspath
 from sys import argv
 
+# Sets paths for mock SQL files
 groupsTableFile = abspath('./test/seeds/music_logger_groups.sql')
 tracksTableFile = abspath('./test/seeds/music_logger_tracks.sql')
 groupsDataFile = abspath('./test/data/groups_data.sql')
 tracksDataFile = abspath('./test/data/tracks_data.sql')
 
+# Sets up connection to database
 connection = pymysql.connect(host=app.config.get("DB_HOST"),
                              user=app.config.get("DB_USER"),
                              password=app.config.get("DB_PASSWORD"),
@@ -17,27 +19,41 @@ connection = pymysql.connect(host=app.config.get("DB_HOST"),
 
 
 def create(args):
-    if any(x.lower() in args for x in ['-h', '--help']):
-        print("Usage:\n  dbsetup [options]\n")
-        print("Options:\n  -e, --erase\t\tResets the database 'Test' without seeding track data\n"
-              "  -s, --seed\t\tAdds track data into the database")
+    if any(x in args for x in ['-h', '--help']):
+        print_help()
+        return
+    if any(x in args for x in ['-s', '--seed']):
+        seed_tracks()
         return
     try:
-        if any(x.lower() in args for x in ['-e', '--erase']):
+        if any(x in args for x in ['-e', '--erase']):
             with connection as cur:
-                cur.execute('DROP DATABASE IF EXISTS test;')
-                cur.execute('CREATE DATABASE test;')
+                cur.execute('DROP DATABASE IF EXISTS music_logger;')
+                cur.execute('CREATE DATABASE music_logger;')
             connection.commit()
+            return
         read_sql_file(connection, groupsTableFile)
         read_sql_file(connection, tracksTableFile)
         read_sql_file(connection, groupsDataFile)
     finally:
         connection.close()
-    if any(x.lower() in args for x in ['-s', '--seed']):
-        seed_tracks()
+
+
+def print_help():
+    """
+    Prints help statement for file usage
+    :return: None
+    """
+    print("\nUsage:\n  dbsetup [options]\n")
+    print("Options:\n  -e, --erase\t\tResets the database 'music_logger' with no tables initialized\n"
+          "  -s, --seed\t\tAdds track data into the database")
 
 
 def seed_tracks():
+    """
+    Populates tracks table with mock data
+    :return: None
+    """
     try:
         read_sql_file(connection, tracksDataFile)
     finally:
@@ -45,6 +61,12 @@ def seed_tracks():
 
 
 def read_sql_file(connect, file):
+    """
+    Helper function to read mock SQL files
+    :param connect: Connection to Database
+    :param file: SQL file to read
+    :return: None
+    """
     with connect.cursor() as cursor:
         with open(file) as f:
             for x in f.read().strip().split(';'):
@@ -53,4 +75,9 @@ def read_sql_file(connect, file):
                     cursor.execute(x)
     connection.commit()
 
-create(argv)
+
+# Converts argv to lowercase for better user input parsing
+arguments = [element.lower() for element in argv]
+
+# Calls main function
+create(arguments)
--- test/data/groups_data.sql
+++ test/data/groups_data.sql
@@ -1,10 +1,14 @@
 --
 -- Dumping data for table `groups`
 --
-
+SET unique_checks = 0;
+SET foreign_key_checks = 0;
 LOCK TABLES `groups` WRITE;
-/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
-INSERT INTO `groups` VALUES (1,'Feature','2012-03-21 18:26:03','2012-03-21 18:26:03'),(2,'New Bin','2012-03-21 18:26:26','2012-03-21 18:26:26'),(3,'Library','2012-03-21 18:26:37','2012-03-21 18:26:37'),(4,'Recurrent','2012-03-21 18:26:48','2012-03-21 18:26:48'),(5,'Specialty Show','2012-03-21 18:27:08','2012-03-21 18:27:08');
-/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
+INSERT INTO `groups` VALUES (1, 'Feature', '2012-03-21 18:26:03', '2012-03-21 18:26:03'),
+  (2, 'New Bin', '2012-03-21 18:26:26', '2012-03-21 18:26:26'),
+  (3, 'Library', '2012-03-21 18:26:37', '2012-03-21 18:26:37'),
+  (4, 'Recurrent', '2012-03-21 18:26:48', '2012-03-21 18:26:48'),
+  (5, 'Specialty Show', '2012-03-21 18:27:08', '2012-03-21 18:27:08');
+SET foreign_key_checks = 1;
+SET unique_checks = 1;
 UNLOCK TABLES;
-
