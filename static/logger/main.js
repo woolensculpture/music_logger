cb4ff77647b36bd3b5b60d5f437f6e16a87524d6
Benjamin Reynolds <benjamin.b.reynolds@gmail.com>
2017-02-11 16:52:26 -0500

    still working on getting the front end javascript where I want it to be, need to get the control and manipulation of the Tracks and the list nailed down, then get the controls working.

--- static/logger/main.js
+++ static/logger/main.js
@@ -3,7 +3,7 @@
  */
 /*TODO refactor for SSE */
 "use strict";
-(function display($, detailed) {
+var display = (function ($, detailed) {
 
     var eventOutputContainer = document.getElementById("event");
     var evtSrc = new EventSource("/subscribe");
@@ -22,43 +22,120 @@
             case 'update':
                 break;
             default:
-                console.error("ERROR: unknown action")
+                console.error("ERROR: unknown action");
+                break;
         }
     };
 
-   function Track(artist, title, time, rvdl, requester, id){
-        this.artist = artist;
-        this.time = Date.parse(time);
-        this.id = id;
-        this.rvdl = rvdl;
-        this.requester = requester ? requester : "";
+    function Track() {
+        if(arguments.length == 7) {
+            //just pass in the parameters
+            this.id = arguments[0];
+            this.artist = arguments[1];
+            this.time = new Date(arguments[2]);
+            this.rvdl = !!(arguments[3]);
+            this.title = arguments[4];
+            this.requester = arguments[5];
+            this.group = arguments[6];
+            this.element = this.toHTML();
+        }
+        else if(arguments.length == 1)
+            if (typeof arguments[0] === 'object') {
 
-        this.createHTML = function(){
-            var trackHTML = "<tr id='" + this.id + "' class='track' >" +
-                "<td class='artist'>" + this.artist + "</td>" +
-                "<td  class='title'>" + this.title + "</td>" +
-                "<td  class='time'>" + this.time + "</td>";
-            if (detailed)
-                trackHTML += "<td  class='rvdl'>" +
-                    (this.rvdl ? "<a class='button tiny disabled round alert'>rvdl</a>" : "") +
-                    "</td>" +
-                    "<td  class='requester'>" + this.requester + "</td>";
-            trackHTML += "</tr>";
-            return trackHTML;
-        };
-
-        this.getTrackClass = function(){
-            if(detailed && this.group != 'Library') {
-                if(this.group.name == 'Feature')
-                    return "table-feature";
-                else if(this.group == 'New Bin')
-                    return "table-newbin";
-            }else {
-                return "table-library";
             }
-        };
-
+            else if(typeof  arguments[0] === 'string'){
+            // pass in raw json
+            }
     }
+
+    Track.prototype.parseFromHTML = function (trackRow) {
+        this.element = trackRow;
+        this.id =this.getField('id');
+        this.artist = this.getField('artist');
+        this.time = new Date(this.getField('time'));
+        this.rvdl = !!(this.getField('rvdl'));
+        this.title = this.getField('title');
+        this.requester = this.getField('requester');
+        this.group = this.getField('group');
+        return this;
+    };
+
+
+    Track.FromJSON = function(json){
+        // TODO probably rebinding the parsed JSON to the Track object is faster and more efficient,
+        // just not confident enough with js to test that it works right now
+
+        var fields = JSON.parse(json);
+        this.toHTML(fields.id, fields.artist, fields.title, fields.time, fields.group, fields.rvdl, fields.requester);
+        return this;
+    };
+
+    Track.prototype.toHTML = function(){
+        var trackHTML = "<tr id='" + this.id + "' class='track " + detailed ? this.getTrackClass():"" + "' >" +
+            "<td class='artist'>" + this.artist + "</td>" +
+            "<td  class='title'>" + this.title + "</td>" +
+            "<td  class='time'>" + this.time + "</td>";
+        if (detailed)
+            trackHTML +=
+                "<td class='group'>" + this.group + "</td>" +
+                "<td  class='rvdl'>" +
+                (this.rvdl ? "<a class='button tiny disabled round alert'>rvdl</a>" : "") +
+                "</td>" +
+                "<td  class='requester'>" + this.requester + "</td>";
+        trackHTML += "</tr>";
+        this.element = $.parseHTML(trackHTML);
+        return this;
+    };
+
+    Track.prototype.toJSON = function(){
+        return JSON.stringify(
+            {
+                artist:this.artist,
+                time:this.time.toString(),
+                rvdl:this.rvdl,
+                title:this.title,
+                group:this.group
+            });
+    };
+
+    Track.prototype.getTrackClass = function(){
+        if(detailed && this.group != 'Library') {
+            if(this.group.name == 'Feature')
+                return "table-feature";
+            else if(this.group == 'New Bin')
+                return "table-newbin";
+        }else {
+            return "table-library";
+        }
+    };
+
+    Track.prototype.getField = function(field) {
+        if(field === 'id') return this.element.id;
+        field = this.element.find("td." + field);
+        return field.length > 0 ? field[0].innerText : '';
+    };
+
+    Track.prototype.setField = function(field, value) {
+        field = this.element.find("td." + field);
+        if(field.length > 0)
+            field[0].innerText = !!(value) ? value : '';
+        return this;
+    };
+
+    var TrackList = {
+        element: $('#logger_body'),
+        arr : $('#logger_body').find('tr').each(function(i,track){return (new Track()).parseFromHTML($(track))}),
+        //TODO
+        add: function(){},
+        remove: function(){},
+        append: function(){},
+        sort: function(){},
+        contains: function(){},
+        find: function(){},
+        foreach: function () {},
+        filter: function () {}
+
+    };
 
     function prependTrack(track){
         var trackHTML = $(createHTML(track));
@@ -86,20 +163,21 @@
 
     function search() {
         $.ajax(window.location.hostname + "/search", {
-            start:$("td#startDate").val(),
-            end:$("td#endDate").val(),
-            artist:$('artistInput'),
-            title:$('titleInput')
+            start: $("td#startDate").val(),
+            end: $("td#endDate").val(),
+            artist: $('artistInput'),
+            title: $('titleInput')
         });
         $(".track").fadeOut("fast").remove();
     }
 
-
     return {
+        Track: Track,
+        TrackList: TrackList,
         addTrack: addTracks,
         updateTrack: updateTrack,
         removeTrack: removeTrack,
         search: search,
         getPage: range
     };
-})(jQuery, detailed);
+}(jQuery, detailed));
